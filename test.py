feb5b354075c9816d50e4e4a5426d717d84b132c
nikgolovko <it2ub@yandex.ru>
2019-03-13 12:17:28 +0300

    Modified version of script

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import cv2
 import dlib
-import pandas as pd
-import numpy as np
 
-cv2.__version__
-dlib.__version__
-pd.__version__
-np.__version__
+print (cv2.__version__)
+print (dlib.__version__)
