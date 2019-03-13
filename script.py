71da51283cd13ff61a4a6b636b7a6d7506e90d9f
nikgolovko <it2ub@yandex.ru>
2019-03-13 16:03:41 +0300

    fix for alert

--- script.py
+++ script.py
@@ -156,7 +156,7 @@ def video_blink(file):
                             t.start()
                         '''
                     print ("Alert")
-                        al = 1
+                    al = 1
                     # draw an alarm on the frame
                         #cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
                         #cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
