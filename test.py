f9b2ff545c2fc0588509248acc569e5d44ea0878
nikgolovko <it2ub@yandex.ru>
2019-03-13 12:23:20 +0300

    Add new libs and their versions

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import cv2
 import dlib
+import pandas as import pd
+import numpy as np
 
-print (cv2.__version__)
-print (dlib.__version__)
+print ("OpenCV version",cv2.__version__)
+print ("Dlib version",dlib.__version__)
+print ("Pandas version",pd.__version__)
+print ("Numpy version",np.__version__)
